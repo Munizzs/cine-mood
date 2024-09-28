4532bcf5d6b204d13d1ea2401941bf49688bd7ca
Camila <camilaasouza2004@gmail.com>
2024-09-27 22:30:11 -0300

    Criação do projeto | Criação do servlet | Criação do banco de dados H2

---
+++ cineMood/src/main/java/br/com/project/cineMood/model/Filme.java
@@ -0,0 +1,14 @@
+package br.com.project.cineMood.model;
+
+public class Filme {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
