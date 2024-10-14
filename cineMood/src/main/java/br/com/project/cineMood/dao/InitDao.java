ffa1a6be5277fa2caacdbf73adb1096a03ea9181
Matheus Muniz <matheusmuniz0810@gmail.com>
2024-10-13 21:01:53 -0300

    Adicionando mais um camibho para conexão com bancos de dados

--- cineMood/src/main/java/br/com/project/cineMood/dao/InitDao.java
+++ cineMood/src/main/java/br/com/project/cineMood/dao/InitDao.java
@@ -8,7 +8,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class InitDao {
-    private static String url = "jdbc:postgresql://localhost:5432/CineFilmes";
+    //private static String url = "jdbc:postgresql://localhost:5432/CineFilmes";
+    private static String url = "jdbc:postgresql://localhost:5432/CineFilme";
     //private static String url = "jdbc:postgresql://3.15.6.30:5432/CineFilmes";
     private static String user = "cineGodness";
     private static String pass = "adaoeva11";
