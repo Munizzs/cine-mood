c6afaaa7d41497fc12f5ee75ce7163c822f2b226
Camila <camilaasouza2004@gmail.com>
2024-10-26 17:59:02 -0300

    + Adicionado a EmocaoDao, para o funcionamento entre o BackEnd e BDD. Metódos createEmocao e findAllEmocao implementados.

---
+++ cineMood/src/main/java/br/com/project/cineMood/dao/EmocaoDao.java
@@ -0,0 +1,70 @@
+package br.com.project.cineMood.dao;
+
+import br.com.project.cineMood.model.Emocao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+public class EmocaoDao {
+
+    public void createEmocao(Emocao emocao)throws SQLException {
+
+        String SQL = "INSERT INTO emocoes (nome, descricao) VALUES (?,?)";
+
+        try {
+            InitDao conex = new InitDao();
+            Connection conn = conex.getConnection();
+
+            PreparedStatement preparedStatement = conn.prepareStatement(SQL);
+            preparedStatement.setString(1, emocao.getNome());
+            preparedStatement.setString(2, emocao.getDescricao());
+
+            preparedStatement.execute();
+            System.out.println("Criada emoção com sucesso!");
+
+            conn.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Falhou na criação da conexão: "+e.getMessage());
+
+        }
+    }
+    public List<Emocao> findAllEmocao() {
+        String SQL = "SELECT * FROM emocoes";
+        try {
+
+            InitDao conex = new InitDao();
+            Connection conn = conex.getConnection();
+            System.out.println("success in database connection (to list)");
+
+            PreparedStatement preparedStatement = conn.prepareStatement(SQL);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<Emocao> emocaoList = new ArrayList<>();
+
+            while(resultSet.next()) {
+                String id = resultSet.getString("id_emocao");
+                String nome = resultSet.getString("nome");
+                String descricao = resultSet.getString("descricao");
+
+                Emocao emocao = new Emocao(id, nome, descricao);
+                emocaoList.add(emocao);
+            }
+
+            System.out.println("Achou a emoção no bdd");
+            conn.close();
+
+            return emocaoList;
+        }catch (Exception e) {
+            System.out.println("Falhou na procura da emoção:"+e.getMessage());
+            return Collections.emptyList();
+        }
+
+    }
+}
