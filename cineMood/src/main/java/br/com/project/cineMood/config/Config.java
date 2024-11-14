265b2f2d1d69b75bcdd36f15855609d47b991620
Matheus Muniz <matheusmuniz0810@gmail.com>
2024-11-14 12:31:22 -0300

    + Classe de configuração

---
+++ cineMood/src/main/java/br/com/project/cineMood/config/Config.java
@@ -0,0 +1,25 @@
+package br.com.project.cineMood.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+    private static final String CONFIG_FILE = "src/main/webapp/resources/config.properties";
+    private static Properties properties;
+
+    static {
+        properties = new Properties();
+        try (FileInputStream input = new FileInputStream(CONFIG_FILE)) {
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Erro: "+ex.getMessage());
+        }
+    }
+
+    public static String getApiKey() {
+        return properties.getProperty("OMDB_API_KEY");
+    }
+}
